5dc90b35aa0fedafb53abfbe6248ca07c520f85d
nakanokurenai <master@yukarium.net>
2018-08-02 02:18:41 +0900

    fix with puppeteer

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ class UQDataCharge {
   getPlans () {
     this.debug('getPlans')
 
-    return this.page.$$('plan-item-name')
+    return this.page.$$('.plan-item-name')
   }
 
   /**
@@ -74,9 +74,12 @@ class UQDataCharge {
     this.debug('getPlanByName')
 
     this.requireLogin()
-    const filtered = await this.page.evaluate((plans, name) => {
-      return plans.filter(v => v.innerText.includes(name))
-    }, this.plans, name)
+    const flags = await Promise.all(
+      this.plans.map(plan => this.page.evaluate((plan, name) => {
+        return plan.innerText.includes(name)
+      }, plan, name)
+    ))
+    const filtered = this.plans.filter((_, index) => flags[index])
     return filtered[0] || null
   }
 
@@ -89,12 +92,15 @@ class UQDataCharge {
     this.requireLogin()
     const plan = await this.getPlanByName(name)
     if (!plan) throw new Error('No plan to be opened.')
-    const button = await this.page.evaluate((plan) => {
+  
+    return this.page.evaluate((plan) => {
       const buttons = plan.parentNode.getElementsByTagName('button')
-      if (buttons > 0) return buttons[0]
-      return
+      if (buttons.length > 0) {
+        buttons[0].click()
+        return true
+      }
+      return false
     }, plan)
-    return button.click()
   }
 
   /**
@@ -110,8 +116,8 @@ class UQDataCharge {
   /**
    * @returns Promise<ElementHandle>
    */
-  getApproveButtonByConfirmDialog(dialog) {
-    this.debug('getApproveButtionByConfirmDialog')
+  approveConfirmDialog(dialog) {
+    this.debug('approveConfirmDialog')
 
     this.requireLogin()
 
@@ -119,8 +125,11 @@ class UQDataCharge {
       const approves = Array.from(dialog.getElementsByTagName('button')).filter(
         v => v.innerText.includes('確定')
       )
-      if (approves.length > 0) return approves[0]
-      return
+      if (approves.length > 0) {
+        approves[0].click()
+        return true
+      }
+      return false
     }, dialog)
   }
 
@@ -129,16 +138,18 @@ class UQDataCharge {
 
     this.requireLogin()
     const dialog = await this.getPurchaseConfirmDialog()
-    const button = await this.getApproveButtonByConfirmDialog(dialog)
-    button.click()
+    return this.approveConfirmDialog(dialog)
   }
 }
 
 async function main() {
   const dc = new UQDataCharge()
+  debug('try to login to', process.env.UQ_AUTO_DATA_CHARGE_USERNAME)
   await dc.login(process.env.UQ_AUTO_DATA_CHARGE_USERNAME, process.env.UQ_AUTO_DATA_CHARGE_PASSWORD)
-  await dc.openPlanByName('まとめてチャージ')
-  await dc.approvePurchase()
+  const open = await dc.openPlanByName('まとめてチャージ')
+  if (!open) throw new Error()
+  const successful = await dc.approvePurchase()
+  debug(`DC is ${successful}`)
   dc.destructor()
 }
 
